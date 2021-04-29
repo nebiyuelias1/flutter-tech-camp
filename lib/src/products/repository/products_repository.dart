7cccc0c20c0e8de8a4745340e0567d5fe3b2a7c3
eliasn <nebiyuelias1@gmail.com>
2021-04-29 07:21:10 +0300

    Consume cubit in products_page.dart

--- lib/src/products/repository/products_repository.dart
+++ lib/src/products/repository/products_repository.dart
@@ -15,7 +15,8 @@ class ProductsRepositoryImpl implements ProductsRepository {
   Future<List<Product>> getAllProducts() async {
     try {
       final response = await _dio.get('https://fakestoreapi.com/products');
-      return response.data.map((p) => Product.fromJson(p)).toList();
+      List<Product> result = response.data.map<Product>((p) => Product.fromJson(p)).toList();
+      return result;
     } on DioError {
       throw NetworkError('Network error');
     } catch (_) {
--- lib/src/products_page.dart
+++ lib/src/products_page.dart
@@ -1,29 +1,56 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_tech_camp/src/products/cubit/products_cubit.dart';
+import 'package:flutter_tech_camp/src/products/models/product.dart';
+import 'package:flutter_tech_camp/src/products/repository/products_repository.dart';
 import 'package:flutter_tech_camp/src/routes/router.gr.dart';
 
 class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Products'),
-        leading: IconButton(
-          onPressed: () {
-            ExtendedNavigator.of(context).push(Routes.profilePage);
-          },
-          icon: Icon(
-            Icons.person,
-            color: Colors.white,
+    return BlocProvider(
+      create: (_) =>
+          ProductsCubit(ProductsRepositoryImpl(Dio()))..getAllProducts(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Products'),
+          leading: IconButton(
+            onPressed: () {
+              ExtendedNavigator.of(context).push(Routes.profilePage);
+            },
+            icon: Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
           ),
         ),
+        body: BlocBuilder<ProductsCubit, ProductsState>(
+          builder: (context, state) {
+            if (state is ProductsInitialState) {
+              return Text('Initial State');
+            } else if (state is ProductsLoadingState) {
+              return Center(child: CircularProgressIndicator());
+            } else if (state is ProductsLoadingSuccessState) {
+              return _Products(products: state.products);
+            } else if (state is ProductsErrorState) {
+              return Text('Error: ' + state.message);
+            }
+
+            return SizedBox();
+          },
+        ),
       ),
-      body: _Products(),
     );
   }
 }
 
 class _Products extends StatelessWidget {
+  final List<Product> products;
+
+  const _Products({Key key, this.products}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
@@ -32,16 +59,19 @@ class _Products extends StatelessWidget {
       ),
       itemCount: 10,
       itemBuilder: (_, index) {
-        return _Product();
+        return _Product(product: this.products[index]);
       },
     );
   }
 }
 
 class _Product extends StatelessWidget {
+  final Product product;
+
+  const _Product({Key key, this.product}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Text('Product Item');
+    return Text('Product: ' + product.title);
   }
 }
-
